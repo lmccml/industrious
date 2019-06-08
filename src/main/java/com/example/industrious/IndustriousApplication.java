089025589a79366016ceeee15b01652cc276c173
lmc <lmc@192.168.0.11>
2019-06-08 17:14:57 +0800

    Initial commit

---
+++ src/main/java/com/example/industrious/IndustriousApplication.java
@@ -0,0 +1,13 @@
+package com.example.industrious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustriousApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndustriousApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/industrious/IndustriousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.industrious;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustriousApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
