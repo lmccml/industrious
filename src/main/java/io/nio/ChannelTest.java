2bb1cedd575baf96c64fbb6cfdc3cdd0f473d624
lmc <347021161@qq.com>
2020-01-05 23:57:19 +0800

    总该有让你不舍昼夜的东东吧

--- src/main/java/io/nio/ChannelTest.java
+++ src/main/java/io/nio/ChannelTest.java
@@ -3,8 +3,10 @@ package io.nio;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.InterruptibleChannel;
 
 /**
  * @author lmc
@@ -17,7 +19,7 @@ public class ChannelTest {
         File out_file = new File(project_path + "/file/out.txt");
 
         FileInputStream fileInputStream = new FileInputStream(file);
-        FileOutputStream fileOutputStream = new FileOutputStream(out_file, true);
+        FileOutputStream fileOutputStream = new FileOutputStream(out_file, false);
 
         //FileChannel它总是运行在阻塞模式下
         FileChannel fileInChannel = fileInputStream.getChannel();
@@ -26,7 +28,7 @@ public class ChannelTest {
         //FileChannel fileOutChannel = new FileOutputStream(file).getChannel();
         //System.out.println(fileInChannel.size());
 
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(30);
         fileInChannel.read(byteBuffer);
         System.out.println(new String(byteBuffer.array(), "UTF-8"));
 
@@ -39,9 +41,24 @@ public class ChannelTest {
         fileOutChannel.write(byteBuffer);
 
         FileChannel fileInChannelNew = new FileInputStream(out_file).getChannel();
-        ByteBuffer byteBufferNew = ByteBuffer.allocate(1024*1024);
-        fileInChannelNew.read(byteBufferNew);
-        System.out.println(new String(byteBufferNew.array(), "UTF-8"));
+        ByteBuffer byteBufferNew = ByteBuffer.allocate(10);
+        ByteBuffer byteBufferNew2 = ByteBuffer.allocate(10);
+        ByteBuffer[] byteBufferArray = new ByteBuffer[]{byteBufferNew, byteBufferNew2};
+        fileInChannelNew.read(byteBufferArray, 0 , 1); //指的是数组的下标，2指的数组长度
+        System.out.println(new String(byteBufferArray[0].array(), "UTF-8"));
+        System.out.println(new String(byteBufferArray[1].array(), "UTF-8"));
+
+        FileChannel fileChannelSrc = new FileInputStream(file).getChannel();
+        FileChannel fileChannelDst = new FileOutputStream(out_file).getChannel();
+        //不用换用，直接一个通道向另一个通道传输字节流
+        fileChannelSrc.transferTo(0, fileChannelSrc.size(), fileChannelDst);
+
+        //RandomAccessFile类是一个专门读写文件的类，封装了基本的IO流，在读写文件内容方面比常规IO流更方便、更灵活。但也仅限于读写文件，无法像IO流一样，可以传输内存和网络中的数据。
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+
+
+
+
 
 
     }
